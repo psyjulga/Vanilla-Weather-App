161569153bbb8772ceb962264d6ac10970be4248
👩Ladybug🐞 <psyjulga@yahoo.de>
2021-05-30 18:58:25 +0200

    displayTemp and formatDate

--- src/app.js
+++ src/app.js
@@ -1,1 +1,46 @@
+function formatDate(timeStamp) {
+  let date = new Date(timeStamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemp(response) {
+  console.log(response.data);
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
+}
+
+let city = "Paris";
+let unit = "metric";
 let apiKey = "521e636942417dbc233358cdf12445eb";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+
+axios.get(apiUrl).then(displayTemp);
